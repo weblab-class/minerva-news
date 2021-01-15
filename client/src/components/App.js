4863e05da666a710f82f13e3994d807696b78cd7
George Tang <gtangg12@Georges-MBP-4.home>
2021-01-15 15:58:25 -0500

    shell

---
+++ client/src/components/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+import NotFound from "./NotFound.js";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: undefined,
+    };
+  }
+
+  componentDidMount() {
+    /*
+    get("/api/whoami").then((user) => {
+      if (user._id) {
+        // they are registed in the database, and currently logged in.
+        this.setState({ userId: user._id });
+      }
+    });*/
+  }
+
+  /*
+  handleLogin = (res) => {
+    console.log(`Logged in as ${res.profileObj.name}`);
+    const userToken = res.tokenObj.id_token;
+    post("/api/login", { token: userToken }).then((user) => {
+      this.setState({ userId: user._id });
+      post("/api/initsocket", { socketid: socket.id });
+    });
+  };
+
+  handleLogout = () => {
+    this.setState({ userId: undefined });
+    post("/api/logout");
+  };*/
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <NotFound default />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
