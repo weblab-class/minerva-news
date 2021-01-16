0d79803519fe93e14039874cbada3ba41d394845
George Tang <gtangg12@Georges-MBP-4.home>
2021-01-16 10:55:43 -0500

    auth

--- server/server.py
+++ server/server.py
@@ -125,5 +125,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' # for local testing only
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)
