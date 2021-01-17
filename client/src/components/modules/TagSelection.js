772d2f1a66b8949a26202c33b313ab10ddab5ccd
uraniumCoder <williamzhaoisawesome@gmail.com>
2021-01-17 18:49:30 -0500

    tagselection interaction

--- client/src/components/modules/TagSelection.js
+++ client/src/components/modules/TagSelection.js
@@ -17,15 +17,36 @@ class TagSelection extends React.Component {
     });
   }
 
+  insertTag = (tag) =>{
+    document.getElementById("taginput").value += tag;
+  };
+
   render() {
     const placeholder_text = "Tag topics you want to explore using #topic...";
     return (
         <div className="tagselection-box u-greybox">
-            <input type="text" placeholder={placeholder_text} className="tagselection-input"/>
+            <input 
+            id="taginput" 
+            type="text" 
+            placeholder={placeholder_text} 
+            className="tagselection-input"
+            onKeyUp={(e) => {
+              e = e || window.event;
+              var keyCode = e.code || e.key;
+              if(keyCode == 'Enter'){
+                var tags = document.getElementById("taginput").value.split('#').slice(1).map((str) => str.trim());
+                document.getElementById("taginput").value = "";
+                this.props.setTags(tags);
+              }
+            }}/>
             <div className="tagselection-suggestbox">
               <div className="tagselection-suggestionlabel">Suggestions: </div>
               {this.state.suggestions.map((suggestion) => (
-                <div className="tagselection-suggestion"> #{suggestion} </div>
+                <div 
+                className="tagselection-suggestion u-plain-button" 
+                onClick={() => {this.insertTag(`#${suggestion} `)}}>
+                  #{suggestion} 
+                </div>
               ))}</div>
         </div>
     );
