9bdef7a60adaf7d39c3f2726ce5077149cf9082b
George Tang <gtangg12@Georges-MBP-4.home>
2021-01-20 18:51:36 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import NotFound from "./pages/NotFound.jsx";
+
+
+class App extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <NotFound />
+    );
+  }
+}
+
+export default App;
