0744bd7300637d5418438f6e562c7d443f2391b5
George Tang <gtangg12@Georges-MBP-4.home>
2021-01-21 01:18:59 -0500

    navbar

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ app = flask.Flask(__name__, static_folder='build/', static_url_path='/')
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/login')
+@app.route('/api/login')
 def login():
-    return json.dumps({'id': '1232132149999'})
+    print("JAHSGDSDGASDGGSJADGh")
+    return json.dumps({'userId': '1232132149999'})
 
-@app.route('/logout')
+@app.route('/api/logout', methods=['POST'])
 def logout():
     return json.dumps({})
+
+app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=False)
